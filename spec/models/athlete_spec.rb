81ffd3d39c8a3bddfc4024ddbd5f94462c999be2
LMafra <chagas.lucas.mafra@gmail.com>
2025-09-02 15:27:50 -0300

    feat(athletes): Create Athletes

---
+++ spec/models/athlete_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+
+RSpec.describe Athlete, type: :model do
+  fixtures :athletes
+
+  describe 'validations' do
+    it 'is valid with valid attributes (from fixtures)' do
+      expect(athletes(:john_doe)).to be_valid
+      expect(athletes(:jane_smith)).to be_valid
+      expect(athletes(:mike_johnson)).to be_valid
+      expect(athletes(:sarah_wilson)).to be_valid
+    end
+
+    it 'requires name' do
+      athlete = Athlete.new(phone: 1234567890, date_of_birth: Date.today)
+      expect(athlete).not_to be_valid
+      expect(athlete.errors[:name]).to include("can't be blank")
+    end
+
+    it 'requires phone' do
+      athlete = Athlete.new(name: 'Test Athlete', date_of_birth: Date.today)
+      expect(athlete).not_to be_valid
+      expect(athlete.errors[:phone]).to include("can't be blank")
+    end
+
+    it 'requires date_of_birth' do
+      athlete = Athlete.new(name: 'Test Athlete', phone: 1234567890)
+      expect(athlete).not_to be_valid
+      expect(athlete.errors[:date_of_birth]).to include("can't be blank")
+    end
+  end
+
+  describe 'attributes' do
+    let(:athlete) { athletes(:john_doe) }
+
+    it 'has the correct name' do
+      expect(athlete.name).to eq('John Doe')
+    end
+
+    it 'has the correct phone' do
+      expect(athlete.phone).to eq(1234567890)
+    end
+
+    it 'has the correct date_of_birth' do
+      expect(athlete.date_of_birth).to eq(Date.parse('1990-05-15'))
+    end
+  end
+
+  describe 'data types' do
+    let(:athlete) { athletes(:jane_smith) }
+
+    it 'stores phone as integer' do
+      expect(athlete.phone).to be_a(Integer)
+      expect(athlete.phone).to eq(9876543210)
+    end
+
+    it 'stores date_of_birth as date' do
+      expect(athlete.date_of_birth).to be_a(Date)
+      expect(athlete.date_of_birth).to eq(Date.parse('1992-08-22'))
+    end
+  end
+end
