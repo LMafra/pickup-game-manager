a32ecc433bdb5be3c3948377efab151f55fe1d9f
LMafra <chagas.lucas.mafra@gmail.com>
2025-09-04 12:43:49 -0300

    feat(dashboard): Create view

--- app/models/income.rb
+++ app/models/income.rb
@@ -2,7 +2,6 @@ class Income < ApplicationRecord
   self.inheritance_column = :_type_disabled
   belongs_to :transaction_category
 
-  validates :type, presence: true
   validates :unit_value, presence: true
   validates :date, presence: true
 
